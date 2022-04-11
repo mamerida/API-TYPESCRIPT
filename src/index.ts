581a7e8538e46d757f2c989000720a09f1aa0cb5
Mario <mamerida2013@gmail.com>
2022-04-11 11:28:58 -0300

    se agrego el ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ app.use(express.json()) //middleware que transforma req.body a un json
 
 const PORT = 3000;
 
-app.get('/ping',(req,res) =>{
+app.get('/ping',(_req,res) =>{ //se coloca el _ y el nombre de la variable para indicar que lo ignore
+
     console.log("hello someone pinged here");
-    res.send('pong')
+    res.send('pong') 
 })
 
 app.listen(PORT , () =>{
     console.log(` el servidor esta en el puerto ${PORT}`) 
-})
+}) 
